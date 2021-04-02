b4cae4c82abf1eed479f607abd59e1634803ab1b
Surjith S M <surjithctly@gmail.com>
2021-04-02 06:24:23 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const STUDIO_REWRITE = {
+  source: '/studio/:path*',
+  destination:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333/studio/:path*'
+      : '/studio/index.html',
+}
+
 module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+   rewrites: () => [STUDIO_REWRITE],
 };
